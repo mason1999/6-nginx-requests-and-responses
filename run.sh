d35be43a84a36c69251d5ba8e631f4043fe4fffb
mason <masonwong1999@gmail.com>
2024-05-04 09:55:51 +1000

    Returning variables to inspect them. (1) Run: curl localhost (2) Run: curl localhost/a (3) Run: curl localhost/hello/world (4) Run: curl localhost/a/b\?foo=bar

---
+++ run.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/bash
+
+getopts ":cdr" option
+
+    case $option in
+        (c)
+            docker compose up -d
+            ;;
+        (d)
+            docker compose down -v --rmi all
+            ;;
+        (r)
+            docker compose down -v --rmi all
+            docker compose up -d
+            ;;
+        (?)
+            echo "no match"
+            ;;
+    esac
